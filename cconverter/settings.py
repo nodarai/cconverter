cf1103d6f88c98632bd4141cfc4a1d31b1d34766
Nodar Kasradze <nod32arai@gmail.com>
2019-10-11 14:19:35 +0200

    Add DRF, rest_auth and activate DRF documentation

--- cconverter/settings.py
+++ cconverter/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_auth',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+}
+
+
+OPENEXCHANGE_APIKEY = os.getenv('OPENEXCHANGE_APIKEY', '')
